1374898e648155c2b105544a6efebe7586ce5435
Shrayan Bhattacharya <83600559+shrayan-bhattacharya@users.noreply.github.com>
2023-11-26 13:52:15 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
-
-global.foodData = require('./db')(function call(err, data, CatData) {
+global.foodData = require("./db")(function call(err, data, CatData) {
   // console.log(data)
-  if(err) console.log(err);
+  if (err) console.log(err);
   global.foodData = data;
   global.foodCategory = CatData;
-})
+});
 
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(
+  cors({
+    origin: ["https://mern-dhaba-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    cedentials: true,
+  })
+);
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
@@ -17,15 +25,14 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/api/auth', require('./Routes/Auth'));
+app.use("/api/auth", require("./Routes/Auth"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
-
+  console.log(`Example app listening on http://localhost:${port}`);
+});
